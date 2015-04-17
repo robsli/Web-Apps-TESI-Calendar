fa33e32a7eaa96f992362f9ebd9893f93474e38b
Robbie Li <robbieli121.zy@gmail.com>
2015-04-16 20:36:00 -0400

    Added Home Page + CSS + Wordpress

---
+++ index.php
@@ -0,0 +1,47 @@
+<!-- Web Applications Project
+  --
+  -- Contributors: Robbie Li, Ayako Mikami, Jonathan Ho
+  --
+  -- index.php
+  -->
+	
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home Page</title>
+	<link rel='stylesheet' type='text/css' href='tesihome.css'/>
+
+
+</head>
+
+<body>
+	<div>
+	<div id='homeleft'>
+		<h1>Menu</h1>
+		<br><br>
+		<form type='get'>
+			<input class='menubutton' type='submit' name='joinlistserv' value='Join Listserv!'/>
+			<br><br><br><br>
+			<input class='menubutton' type='submit' name='eventcal' value='Event Calendar'/>
+			<br><br><br><br>
+			<input class='menubutton' type='submit' name='news' value='Tech News'/>
+			<br><br><br><br>
+			<input class='menubutton' type='submit' name='about' value='About'/>
+			<br><br><br><br>
+		</form>
+	</div>
+	<div id='homeright'>
+		<h1>Welcome to the Boston College Technology, Entrepreneurship, and Social Innovation Calendar</h1>
+		<br><br>
+		<ul>List of Contributors:
+			<li> Robbie Li </li>
+			<li> Ayako Mikami </li>
+			<li> Jonathan Ho </li>
+		</ul>
+	</div>
+	</div>
+
+
+</body>
+
+</html>
