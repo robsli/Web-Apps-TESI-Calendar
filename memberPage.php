b39392ff2abdc00fe63a6aa8d43e02a19aef2a05
mikamia <ayakomikami11@gmail.com>
2015-05-06 04:38:37 -0400

    member page updated
    still need to display all the events person is attending

--- memberPage.php
+++ memberPage.php
@@ -26,8 +26,58 @@ include('include/dbconn.php');
 ?>
 <!-- Beginning of Content in Body-->
 	<div class="container">
-	<h1> Member Page</h1>
+	
+	<?php
+	$id = isset($_SESSION['userlogin'])?$_SESSION['userlogin']:"error: not logged in";
+	$name = isset($_SESSION['firstname'])?$_SESSION['firstname']:"error: not logged in";
+	echo "<h2>Hello $name! This is your profile page</h2>
+	<br>
+	<h4>Account Settings</h4>";
+	
+	?>
+	
 	<a href="changePassForm.php">Change Password</a>
+	<br>
+	<a href="#">Delete Account</a>
+	<br><br>
+	<h4>Profile</h4>
+	<?php
+		$dbc= connectToDB("lifm");
+		$query1 = "SELECT * from TESI_MEMBERSHIP where email = '$id'"; 
+		$result1 = performQuery($dbc, $query1);
+	    if (mysqli_num_rows($result1) == 1) {
+     		 $row = mysqli_fetch_assoc($result);
+     		 $email = $row['email'];
+     		 $firstname = $row['firstname'];
+     		 $date = $row['registrationdate'];
+     		 $lastname = $row['lastname'];
+     		 $school = $row['school'];
+     		 $class = $row['classyear'];
+     		 $major = $row['major'];
+			 $type = $row['membershiptype'];
+		}
+			 echo "Name: $firstname $lastname<br>";
+			 echo "Meber Since: $date<br>";
+			 echo "School: $school<br>";
+			 echo "Class: $class<br>";
+			 echo "Major: $major<br>";
+			 echo "Membership Type: $type<br>";
+		
+		//$query2 = "select event_name from TESI_EVENTS e, TESI_ATTENDANCE a where e.id = a.id and a.memberID = $id"; 
+		$query2 = "select * from TESI_EVENTS e
+					join
+					TESI_ATTENDANCE a
+					where e.id = a.id
+					and a.memberID = $id";
+		$result2 = performQuery($dbc, $query2);
+	    if (mysqli_num_rows($result2) != 0) {
+	    	print_r($result2);
+	    }
+			 ?>
+			 <br><br>
+	  <h4>Events You Signed up For</h4>
+	  
+	  
 	</div>
 	
 </body>
